b7dab2fd440e8fac8826b25affe738e04b83fee3
Sergey K <89184083452.mail.ru>
2020-08-01 18:34:06 +0300

    изменение температуры кондиционера

---
+++ src/main/java/ru/netology/Conditioner.java
@@ -0,0 +1,46 @@
+package ru.netology;
+
+import java.util.Stack;
+
+public class Conditioner {
+    private String name;
+    private int maxTemperature;
+    private int minTemperature;
+    private int currentTemperature;
+    private boolean on;
+
+    public int getMaxTemperature() {return maxTemperature;}
+    public void setMaxTemperature(int maxTemperature) {
+        this.maxTemperature = maxTemperature;
+        increaseCurrentTemperature();}
+    public int getMinTemperature() {return minTemperature;}
+    public void setMinTemperature(int minTemperature) {
+        this.minTemperature = minTemperature;
+        decreaseCurrentTemperature();}
+    public int getCurrentTemperature() {return currentTemperature;}
+    public void setCurrentTemperature(int currentTemperature) {this.currentTemperature = currentTemperature;}
+
+    public void increaseCurrentTemperature() {
+        for (int temp = 1; ; temp++) {
+            if (currentTemperature >= maxTemperature) {
+                return;
+            }
+            currentTemperature++;
+            System.out.print(currentTemperature+",");
+            this.maxTemperature = maxTemperature;
+            this.currentTemperature = currentTemperature;
+        }
+    }
+    public void decreaseCurrentTemperature() {
+        for (int temp = 1; ; temp++) {
+            if (currentTemperature <= minTemperature) {
+                return;
+            }
+            currentTemperature--;
+            System.out.print(currentTemperature+",");
+            this.minTemperature = minTemperature;
+            this.currentTemperature = currentTemperature;
+        }
+    }
+}
+
---
+++ src/test/java/ru/netology/ConditionerTest.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConditionerTest {
+
+    @Test
+    public void increaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setCurrentTemperature(18);
+        conditioner.setMaxTemperature(25);
+        assertEquals(conditioner.getMaxTemperature(), conditioner.getCurrentTemperature());
+    }
+
+    @Test
+    public void decreaseCurrentTemperature() {
+        Conditioner conditioner = new Conditioner();
+        conditioner.setCurrentTemperature(20);
+        conditioner.setMinTemperature(16);
+        assertEquals(conditioner.getMinTemperature(), conditioner.getCurrentTemperature());
+    }
+}
